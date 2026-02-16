3e1d32e20eac805ac734254c93962e79ff3f158c
Rohit K. Shaw <shawrhit@outlook.com>
2026-02-16 12:03:04 +0530

    allowed frontend to access

--- backend_core/settings.py
+++ backend_core/settings.py
@@ -117,7 +117,7 @@ CSRF_TRUSTED_ORIGINS = [
     origin.strip()
     for origin in os.getenv(
         'CSRF_TRUSTED_ORIGINS',
-        'http://127.0.0.1:8000,http://localhost:8000,http://127.0.0.1:5173,http://localhost:5173'
+        'http://127.0.0.1:8000,http://localhost:8000,http://127.0.0.1:5173,http://localhost:5173,https://gdgnehu.pages.dev/'
     ).split(',')
     if origin.strip()
 ]
